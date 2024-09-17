4679f871a06e42d6c6ee0418147976fca979ec01
darmond157 <bardiarezaeikalantari@gmail.com>
2024-09-17 15:20:50 +0330

    chore: configure cors

--- Server/src/plugins/cors.js
+++ Server/src/plugins/cors.js
@@ -1,6 +1,7 @@
 module.exports = async (fastify) => {
 	await fastify.register(require("@fastify/cors"), {
 		origin: "*",
+		methods: ["GET", "POST"],
 	});
 	fastify.after((err) => {
 		if (err) {
