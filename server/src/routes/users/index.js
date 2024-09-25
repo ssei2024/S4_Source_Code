4794c0d0943b38e1015594715246f5a810d2db51
darmond157 <bardiarezaeikalantari@gmail.com>
2024-09-25 11:55:06 +0330

    feat: add new CRUD routes to users

--- server/src/routes/users/index.js
+++ server/src/routes/users/index.js
@@ -1,13 +1,18 @@
 const countUsersHandler = require("../../handlers/users/CountUsers");
 const createUserHandler = require("../../handlers/users/CreateUser");
+const editUserHandler = require("../../handlers/users/editUserHandler");
+const getUserHandler = require("../../handlers/users/getUserHandler");
+const getUsersHandler = require("../../handlers/users/getUsersHandler");
+const deleteUsersHandler = require("../../handlers/users/deleteUsersHandler");
 
 const createUserSchema = require("../../schemas/users/CreateUser");
+const editUserSchema = require("../../schemas/users/CreateUser");
 
 module.exports = (fastify, opts, done) => {
 	const routes = [
 		{
 			method: "POST",
-			url: "",
+			url: "/",
 			handler: createUserHandler(fastify),
 			schema: createUserSchema,
 		},
@@ -15,6 +20,27 @@ module.exports = (fastify, opts, done) => {
 			method: "GET",
 			url: "/count",
 			handler: countUsersHandler(fastify),
+		},
+		{
+			method: "PATCH",
+			url: "/",
+			handler: editUserHandler(fastify),
+			schema: editUserSchema,
+		},
+		{
+			method: "GET",
+			url: "/:id",
+			handler: getUserHandler(fastify),
+		},
+		{
+			method: "GET",
+			url: "/",
+			handler: getUsersHandler(fastify),
+		},
+		{
+			method: "DELETE",
+			url: "/",
+			handler: deleteUsersHandler(fastify),
 		},
 	];
 
